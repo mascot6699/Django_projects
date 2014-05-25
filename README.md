7694e6ab4c43c2eab443a004f749e74b809a3f62
Umang windows <umangshucool@gmail.com>
2014-05-25 20:30:42 +0530

    till part six done

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ Django_projects
 ===============
 
 Learning to use django framework
+
+
+Requirements: python 2.7
+
+
+How to use it
+================
+
+pip install Django==1.5.4
+cd mysite
+python manage.py runserver
--- django tutorial official/mysite/mysite/settings.py
+++ django tutorial official/mysite/mysite/settings.py
@@ -68,7 +68,7 @@ STATIC_ROOT = ''
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
+STATICFILES_DIRS = ("C:/Users/Umang/Documents/try/mysite/polls/static",
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
