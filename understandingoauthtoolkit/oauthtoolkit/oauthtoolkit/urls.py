48854f87bfd37a73bd7773775bad923fcfdc1226
Umang Shukla <umang.shukla@kuliza.com>
2015-06-22 14:38:14 +0530

    Finishes tutorial

---
+++ understandingoauthtoolkit/oauthtoolkit/oauthtoolkit/urls.py
@@ -0,0 +1,28 @@
+"""oauthtoolkit URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    url(r'^accounts/login/$', auth_views.login, {'template_name': 'login.html'}),
+    url(r'^api/hello/', views.ApiEndpoint.as_view()),
+    url(r'^secret/$', views.secret_page, name='secret'),
+
+]
